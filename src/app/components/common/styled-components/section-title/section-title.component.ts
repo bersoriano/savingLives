56d642c7ff8040650462e4e9412cdcced75ec76b
Bernardo Soriano <bersoriano@me.com>
2017-09-22 09:34:15 -0400

    section title component

--- src/app/components/common/styled-components/section-title/section-title.component.ts
+++ src/app/components/common/styled-components/section-title/section-title.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-title',
+  inputs: ['titleName'],
   templateUrl: './section-title.component.html',
   styleUrls: ['./section-title.component.scss']
 })
 export class SectionTitleComponent implements OnInit {
-
+  titleName: String;
   constructor() { }
 
   ngOnInit() {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   cities: Array<String> = ['Ciudad de Mexico CDMX', 'Puebla', 'Morelos', 'Otros'];
-
+  title: String = 'SELECCIONA UNA CIUDAD';
   constructor() { }
 
   ngOnInit() {
