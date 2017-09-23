7936f9126773826180ce8a482aac5c60a0181a8f
Bernardo Soriano <bersoriano@me.com>
2017-09-23 17:49:35 -0400

    added collapsibe list

---
+++ src/app/components/common/styled-components/collapsible-list/collapsible-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collapsible-list',
+  templateUrl: './collapsible-list.component.html',
+  styleUrls: ['./collapsible-list.component.scss']
+})
+export class CollapsibleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
